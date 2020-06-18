88c1fbd4de5362a827313709755479a62e81a470
sbarrow825 <sbarrow825@berkeley.edu>
2020-06-18 16:02:53 -0700

    added getPasswordLength() function

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passswordLength;
+var characterTypes;
+var inputLength;
+var lowercase;
+var uppercase;
+var numeric;
+var special;
+var atLeastOne;
 
 // Write password to the #password input
 function writePassword() {
@@ -8,6 +16,30 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+function generatePassword() {
+  passswordLength = getPasswordLength();
+  characterTypes = getCharacterTypes();
+}
+
+function getPasswordLength() {
+  inputLength = prompt("Please enter a number to choose the length of your password (Note: must be no less than 8 but no greater than 128)");
+  if (inputLength > 128 || inputLength < 8) {
+    alert("Invalid input, please enter a length no less than 8 but no greater than 128 for your password");
+    return getPasswordLength();
+  }
+  else {
+    return inputLength;
+  }
+}
+
+function getCharacterTypes() {
+  lowercase = confirm("Do you want to include lowercase letters in your password?");
+  uppercase = confirm("Do you want to include uppercase letters in your password?");
+  numeric = confirm("Do you want to include numbers in your password?");
+  special = confirm("Do you want to include special characters in your password?");
+  if (!lowercase && !uppercase && )
 }
 
 // Add event listener to generate button
