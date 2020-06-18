3c3f544dd4118f2923aca00ada4803b59cc382c4
sbarrow825 <sbarrow825@berkeley.edu>
2020-06-18 16:22:58 -0700

    added arrays for special characters, numbers, lowercase, and uppercase

--- Develop/script.js
+++ Develop/script.js
@@ -8,7 +8,12 @@ var uppercase;
 var numeric;
 var special;
 var atLeastOne;
+var specialCharactersArray =  ["\!", "\"", "\#", "\%", "\&", "\'", "\(", "\)", "\*", "\+", "\-", "\.", "\/", "\:", "\;", "\<", "\=", "\>", "\?", "\@", "\[", "\\", "\]", "\^", "\_", "\`", "\{", "\|", "\}", "\~"];
+var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+console.log(specialCharacters[0]);
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
