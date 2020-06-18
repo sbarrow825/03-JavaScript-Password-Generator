ee8438049c01d5cd793cb8ec8f415345c72e0dc1
sbarrow825 <sbarrow825@berkeley.edu>
2020-06-18 16:35:02 -0700

    added getIncludedCharacterTypes() function

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var password;
+var passwordText;
 var passswordLength;
 var characterTypes;
+var getIncludedCharacters;
+var includedCharacters = [];
 var inputLength;
 var lowercase;
 var uppercase;
@@ -12,12 +16,13 @@ var specialCharactersArray =  ["\!", "\"", "\#", "\%", "\&", "\'", "\(", "\)", "
 var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var allPossibleCharacters = [specialCharactersArray, numbersArray, lowercaseArray, uppercaseArray];
 
 console.log(specialCharacters[0]);
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  password = generatePassword();
+  passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -26,6 +31,8 @@ function writePassword() {
 function generatePassword() {
   passswordLength = getPasswordLength();
   characterTypes = getCharacterTypes();
+  includedCharacters = getIncludedCharacters(characterTypes);
+
 }
 
 function getPasswordLength() {
@@ -51,6 +58,15 @@ function getCharacterTypes() {
   else {
     return [lowercase, uppercase, numeric, special];
   }
+}
+
+function getIncludedCharacters(characterTypes) {
+  for (i = 0; i < characterTypes.length; i += 1) {
+    if (characterTypes[i]) {
+      includedCharacters.concat(allPossibleCharacters[i]);
+    }
+  }
+  return includedCharacters;
 }
 
 // Add event listener to generate button
